abd77a19562b3c149da9d3c9f47109a6dcbb4f48
pranaysj <pranaysj3@gmail.com>
2024-11-13 13:15:01 +0530

    Player Controller
    Added playerController script and make the player input

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator animator;
+
+    void Update()
+    {
+        float speed = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("Speed", Mathf.Abs(speed));
+
+        Vector3 scale = transform.localScale;
+        if (speed < 0)
+        {
+            scale.x = -1.0f * Mathf.Abs(scale.x);
+        }
+        else if (speed > 0)
+        {
+            scale.x = Mathf.Abs(scale.x); 
+        }
+        transform.localScale = scale;
+    }
+}
