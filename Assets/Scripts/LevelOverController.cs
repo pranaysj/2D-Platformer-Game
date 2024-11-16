e4d4e90b2e6fd8ab4a0c010bf81903122f886547
pranaysj <pranaysj3@gmail.com>
2024-11-16 08:48:33 +0530

    Level Unlocking system with 4 Unique levels
    [Challenge] - Create 5 unique levels that once unlocked the player can directly jump into from the lobby.

--- Assets/Scripts/LevelOverController.cs
+++ Assets/Scripts/LevelOverController.cs
@@ -1,16 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelOverController : MonoBehaviour
 {
+    public GameObject levelCompletePanel;
+    public Button lobbyButton;
+
+    private void Awake()
+    {
+        lobbyButton.onClick.AddListener(Lobby);
+    }
+
+    private void Lobby()
+    {
+        SceneManager.LoadScene("Lobby");
+    }
+
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.GetComponent<PlayerController>() != null)
         {
             Debug.Log("Level is Complete!");
             LevelManager.Instance.MakerCurrentLevelComplete();
+            levelCompletePanel.SetActive(true);
         }
-
     }
+
 }
